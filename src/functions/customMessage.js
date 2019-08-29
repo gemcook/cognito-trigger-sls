024a55b9e8cbb5435c52cb01ddab6e335f46f6f2
Takuya Fujimoto <t.fujimoto@gemcook.com>
2019-08-29 16:05:23 +0900

    [update] sub

--- src/functions/customMessage.js
+++ src/functions/customMessage.js
@@ -5,16 +5,21 @@ export const customMessage = (event, config) => {
   return new Promise((resolve, reject) => {
     const _event = event;
     const username = Base64.encode(_event.userName);
-    const mail = Base64.encode(_event.request.userAttributes.email);
+    const sub = Base64.encode(_event.userSub);
+    const email = Base64.encode(_event.request.userAttributes.email);
 
     console.log(_event);
+
+    console.log(username);
+    console.log(sub);
+    console.log(email);
 
     try {
       switch (_event.triggerSource) {
         // SignUp
         case 'CustomMessage_ResendCode':
         case 'CustomMessage_SignUp': {
-          const link = `${config.signUpRedirectPath}?username=${username}&email=${mail}`;
+          const link = `${config.signUpRedirectPath}?username=${username}&email=${email}&sub=${sub}`;
           const email = createSignUpEmail(_event, link, config);
 
           _event.response.emailSubject = `${config.prefix}${email.subject}`;
